e557b526b1f29f9a38ec664011f75121060eaea8
chetan316 <chetanpadalkar2008@gmail.com>
2018-06-14 00:47:05 +0530

    New Project Added

---
+++ EkRishta/Models/Login.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace Models
+{
+    public class Login
+    {
+        [Required(ErrorMessage = "Please Enter LoginId")]
+        public string LoginId { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Password")]
+        [DataType(DataType.Password)]
+        public string LoginPassword { get; set; }
+
+        [Required(ErrorMessage = "Please Enter OTP")]
+        public string LoginOTP { get; set; }
+    }
+}
